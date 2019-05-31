b212c146d5a9a8be6b3d36c7790c5288d23d5510
hua chen yu <929762292@qq.com>
2019-05-31 23:06:52 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div ref="editor" style="height: 500px"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import 'quill/dist/quill.core.css'
+  import 'quill/dist/quill.snow.css'
+  import 'quill/dist/quill.bubble.css'
+  import $Quill from './plugins/quill'
+  const Quill = window.Quill || $Quill
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      content: '',
+      options: {
+        upload: '/api/igloo',
+        theme: 'snow',
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ 'header': 1 }, { 'header': 2 }],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'script': 'sub' }, { 'script': 'super' }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }],
+            [{ 'direction': 'rtl' }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+            ['clean'],
+            ['link', 'image', 'video']
+          ]
+        },
+      }
+    }
+  },
+  mounted() {
+    this.quill = new Quill(this.$refs.editor, this.options)
+    this.quill.on('text-change', () => {
+      let html = this.$refs.editor.children[0].innerHTML
+      if (html === '<p><br></p>') html = ''
+      this.content = html
+    })
+  },
+  beforeDestroy() {
+    this.content = ''
+    this.quill = null
+    delete this.quill
+  },
 }
 </script>
 
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// vue.config.js
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:7001/',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                },
+            }
+        }
+    }
+}
